d1090b8adf0163b9e044ea618b77790b1f4b5f19
fifi1ous <137277628+fifi1ous@users.noreply.github.com>
2024-10-15 13:55:32 +0200

    Cviceni 15.10.2024

--- U2.m
+++ U2.m
@@ -1,1 +1,18 @@
-clc; clear; foramt long G
+clc; clear; format long G
+
+im=imread('MMC14_sk3.jpg');
+
+imR=im(:,:,1);
+imG=im(:,:,2);
+imB=im(:,:,3);
+
+[r,c] = size(im);
+
+Rect=[6386        1891          32          80
+      1607	      2033	        33	        76
+      3320        5462          34          80];
+% Interaktivní výběr vzorku
+figure(1)
+ImR = imshow(imR)
+[J,rect] = imcrop(ImR);
+
