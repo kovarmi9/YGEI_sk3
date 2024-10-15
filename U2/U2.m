0ee2d46d226976ff760fb96f095699e8cf3011a0
kovarmi9 <125283780+kovarmi9@users.noreply.github.com>
2024-10-15 19:19:27 +0200

    Update U2.m
    added comments

--- U2/U2.m
+++ U2/U2.m
@@ -1,18 +1,21 @@
-clc; clear; format long G
+clc; clear variables; close all; format long g
 
+% Load the image
 im=imread('MMC14_sk3.jpg');
 
+% Getting size of rows columns and depth from 3D matricx
+[row, col, dep] = size(im);
+
+% Extract RGB components
 imR=im(:,:,1);
 imG=im(:,:,2);
 imB=im(:,:,3);
 
-[r,c] = size(im);
-
-Rect=[6386        1891          32          80
-      1607	      2033	        33	        76
-      3320        5462          34          80];
-% Interaktivní výběr vzorku
-figure(1)
-ImR = imshow(imR)
-[J,rect] = imcrop(ImR);
-
+% Rect=[6386        1891          32           80
+%       1607        2033          33           76
+%       3320        5462          34           80];
+% 
+% % Interaktivní výběr vzorku
+% figure(1)
+% ImR = imshow(imR);
+% [J,rect] = imcrop(ImR);
