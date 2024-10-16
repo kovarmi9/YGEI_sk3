a520b9a1fada1dafc55d01772f968642f16bf0dd
fifi1ous <phill.roucka@gmail.com>
2024-10-16 17:42:08 +0200

    Resample up
    from smaller to bigger with the nearest neighbour

--- U1/Resample.m
+++ U1/Resample.m
@@ -1,20 +1,27 @@
 clc; clear; format long G
 
-MyImg=randi(255,128,128,3);
+MyImg=randi(255,8,8,3);
 
 
+[r,~,b]=size(MyImg);
+k=128;
+nS=k/r;
+nR=k/r;
 
-k=4;
-
-[r,s,b]=size(MyImg);
 NewImg=zeros(k,k,b);
-nS=r/k;
-nR=r/k;
-
-for n=1:b    
-    for i=0:k-1
-        for j=0:k-1
-            NewImg(i+1,j+1,n)=MyImg(i*nS+1,j*nR+1,n);
-        end
+for n=1:b
+    for i=0:r-1
+       for j=0:r-1
+           NewImg(i*nR+1:i*nR+nR,j*nS+1:j*nS+nS,n)=MyImg(i+1,j+1,n);
+       end
     end
 end
+
+
+% for n=1:b    
+%     for i=0:k-1
+%         for j=0:k-1
+%             NewImg(i+1,j+1,n)=MyImg(i*nS+1,j*nR+1,n);
+%         end
+%     end
+% end
