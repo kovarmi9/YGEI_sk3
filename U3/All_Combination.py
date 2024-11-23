c17c541ee13cec329aaffca607bc09417c6683a7
fifi1ous <phill.roucka@gmail.com>
2024-11-23 12:44:22 +0100

    Bonus 2: shortest path between all nodes
    Bug fixed, and upgraded Dijkstra Algorithm

---
+++ U3/All_Combination.py
@@ -0,0 +1,92 @@
+from math import inf
+from queue import PriorityQueue
+
+G = {
+    1 : {2:8, 3:4, 5:2},
+    2 : {1:8, 3:5, 4:2, 7:6, 8:7},
+    3 : {1:4, 2:5, 6:3, 7:4},
+    4 : {2:2, 9:3},
+    5 : {1:2, 6:5},
+    6 : {3:3, 5:5, 7:5, 8:7, 9:10},
+    7 : {2:6, 3:4, 6:5, 8:3},
+    8 : {2:7, 6:7, 7:3, 9:1},
+    9 : {4:3, 6:10, 8:1}
+}
+
+# Coordinates for plotting
+C = {
+    1 : [95, 322],
+    2 : [272, 331],
+    3 : [173, 298],
+    4 : [361, 299],
+    5 : [82, 242],
+    6 : [163, 211],
+    7 : [244, 234],
+    8 : [333, 225],
+    9 : [412, 196]
+}
+
+def rec(u, v, P):
+    path = []
+    
+    # Path shortening
+    while v != u and v != -1:
+        path.append(v)
+        v = P[v]
+        
+    path.append(v)
+    if v != u:
+        print('Incorrect path')
+    return path
+
+def dijkstra(G, u, v):
+    # Input data structures
+    n = len(G)
+    P = [-1] * (n + 1)
+    D = [inf] * (n + 1)
+    PQ = PriorityQueue()
+    
+    # Starting node, add to PQ
+    PQ.put((0, u))
+    D[u] = 0
+    
+    while not PQ.empty():
+        du, u = PQ.get()
+        
+        # Optional: Early stopping if we reached target
+        if u == v:
+            break
+            
+        # Skip if we found a better path already
+        if du > D[u]:
+            continue
+            
+        # Iterate through neighbours
+        for neighbor, weight in G[u].items():
+            # Edge relaxation: path to neighbor through u is better
+            if D[neighbor] > D[u] + weight:
+                D[neighbor] = D[u] + weight
+                P[neighbor] = u
+                PQ.put((D[neighbor], neighbor))
+                
+    return P, D[v] # Return full distance array instead of just D[v]
+
+P, dmin = dijkstra(G, 1, 9)
+
+path = rec(1,2,P)
+print(path,dmin)
+
+def allPaths(G):
+    paths = {}
+    keys = list(G.keys())
+    for i in range(len(keys)):
+        for j in range(i + 1, len(keys)):
+            P, dmin = dijkstra(G, keys[i], keys[j])
+            path = rec(keys[i], keys[j], P)
+            paths[keys[i], keys[j]] = (path, dmin)
+
+    return paths
+
+
+paths = allPaths(G)
+print(paths)
