e7d5421429195214df1c50420c9ac2ec418af554
Michal Kovář <kovarmi9@seznam.cz>
2024-11-24 12:16:16 +0100

    Created weighted graphs

--- U3/test_graph_reader.py
+++ U3/test_graph_reader.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 # Importing the function from graph_reader.py
 from graph_reader import read_graph  # type: ignore
 
@@ -19,3 +20,27 @@ G, C = read_graph(file)
 # Printing the graph
 print(G)
 print(C)
+
+# Plotting the graph
+def PlotGraph(G, C):
+    plt.figure(figsize=(8, 6))
+    
+    # Plot the nodes
+    for node, (x, y) in C.items():
+        plt.scatter(x, y, color='red')
+        plt.text(x+5, y, str(node), fontsize=12, ha='left')
+    
+    # Plot the edges
+    for node, neighbors in G.items():
+        x_start, y_start = C[node]
+        for neighbor in neighbors:
+            x_end, y_end = C[neighbor]
+            plt.plot([x_start, x_end], [y_start, y_end], 'k-', lw=1)  # Line between nodes
+    
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.grid(True)
+    plt.show()
+
+# Plot the graph
+PlotGraph(G, C)
